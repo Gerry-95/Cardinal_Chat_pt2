1fbfb93c03afa3037e021af2a21475c41ff8d4c5
Gerry-95 <scott.rusk@greenwichschools.org>
2017-03-29 10:05:32 -0400

    Add files via upload

--- js/Cardinal Java.js
+++ js/Cardinal Java.js
@@ -47,5 +47,34 @@ function commentate() {
 };
 
 function postForm() {
-    console.log("thing");
+    // Create a root reference
+    var filename = document.getElementByName("Title").innerHTML
+    var storageRef = firebase.storage().ref('/userPosts/' + filename);
+    var uploadTask = storageRef.child.put(document.getElement("form").innerHTML);
+    
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      // Handle unsuccessful uploads
+    }, function() {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      var downloadURL = uploadTask.snapshot.downloadURL;
+});
+    
 };
