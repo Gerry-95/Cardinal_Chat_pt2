7e4a5275a099010a774ef448c6192cd17a779ead
Gerry-95 <scott.rusk@greenwichschools.org>
2017-03-31 13:51:19 -0400

    Add files via upload

--- js/Cardinal Java.js
+++ js/Cardinal Java.js
@@ -25,7 +25,7 @@ function signIn() {
     // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
     // ...
-});
+    });
 }
 
 function showHomePage() {
@@ -48,35 +48,8 @@ function commentate() {
 };
 
 function postForm() {
-    // Create a root reference
-    selectedFile = document.getElement("form").innerHTML;
-    var filename = selectedFile
-    var storageRef = firebase.storage().ref('/userPosts/' + filename);
-    var uploadTask = storageRef.child.put();
-    
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed', function(snapshot){
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function(error) {
-      // Handle unsuccessful uploads
-    }, function() {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      var downloadURL = uploadTask.snapshot.downloadURL;
-});
-    
+    var message = document.getElementById("postfield").innerHTML;
+    ref.putString(message).then(function(snapshot) {
+        console.log("Uploaded a raw string");
+    });
 };
